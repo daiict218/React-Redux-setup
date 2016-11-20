0c15cb2d912626135f7ea70c7bf7a882cb200d69
Ajay Gaur <daiict218@gmail.com>
2016-11-20 17:25:22 +0530

    added eslint dependencies in package.json and preloaders to webpack config file and airbnb eslint rules to .eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -16,15 +16,25 @@ module.exports = {
     new webpack.NoErrorsPlugin()
   ],
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ],
     loaders: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
         excludes: /node_modules/,
         query: {
-          presets: [ 'react' , 'es2015' , 'react-hmre']   //query will tell what bable-loader has to do, which is compiling JSX(react) and es2015
+          presets: ['react', 'es2015', 'react-hmre']   //query will tell what bable-loader has to do, which is compiling JSX(react) and es2015
         }
       }
     ]
+  },
+  eslint: {
+    configFile: './.eslintrc'
   }
-}
+};
